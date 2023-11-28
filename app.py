7c4f97508ec7abebd8563481ed3987f727c97fef
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-11-28 08:25:32 +0800

    add CORS settings

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import product, homework
 from db import models
 from db.database import engine
@@ -21,6 +22,20 @@ def root():
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, reload=True)
+
+origins = [
+    'http://localhost:3000',
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
+)
+
 
 
 models.Base.metadata.create_all(engine)
